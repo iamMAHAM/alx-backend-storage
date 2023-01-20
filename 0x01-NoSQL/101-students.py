36e306492a67d850bb8c8685763aff6d10624970
iamMAHAM <abdulkabore@gmail.com>
2023-01-20 11:37:20 -0800

    advanced tasks

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+return all students sorted by score
+"""
+
+
+def top_students(mongo_collection):
+    """
+    List all students sorted by score
+    """
+    return mongo_collection.aggregate([
+        {
+            '$project': {
+                'name': '$name',
+                'averageScore': {
+                    '$avg': "$topics.score"
+                }
+            }
+        },
+        {
+            '$sort': {
+                "averageScore": -1
+            }
+        }
+    ])
